3a0f66876836b7becc92f1ec1929781042fc2e59
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-07-02 00:16:54 +0530

    first commit

---
+++ oops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;    
+
+int main(){
+    // Declare a string variable
+    string str = "Hello, World!";
+    
+    // Output the string to the console
+    cout << str << endl;
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
