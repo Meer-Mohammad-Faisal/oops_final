aca63502eafbd701834854f8268a8ee032953925
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-07-10 18:32:12 +0530

    static keyword

--- 9_static_keyword/static.cpp
+++ 9_static_keyword/static.cpp
@@ -7,10 +7,63 @@ void fun(){
     x++;
 }
 
+class A{
+    public:
+        int x;
+
+        void inc(){
+            x = x + 1;
+        }
+};
+
+
+class ABC{
+    // checking the ordering of constructor and destructor
+    public:
+        ABC(){
+            cout << "I am constructor"<<endl;
+
+        }
+
+        ~ABC(){
+            cout << "I am destructor"<<endl;
+        }
+};
+
 int main(){
 
-    fun();
-    fun();
-    fun();
+    A obj1;
+    A obj2;
+    obj1.x = 10;
+    obj2.x = 20;
+    cout << obj1.x << endl;
+    obj1.inc();
+    cout << obj1.x << endl;
 
+    cout << obj2.x << endl;
+    obj2.inc();
+    cout << obj2.x << endl;
+
+
+
+
+
+    if(true){
+        static ABC object; // using static keyword. it will saved as lifetime in program
+                           // when the complete main funciton end. then only, destructor function will called.
+    
+
+    }
+    cout << "End of Main Function"<<endl;
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+// Explore friend function and friend class.
